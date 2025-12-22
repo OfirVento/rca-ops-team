de429041c1e4fdb7750b9103c7c23c676dda684f
Ofir Co <ofirco@Ofirs-MacBook-Pro.local>
2025-12-22 19:06:50 +0200

    Manual Next.js configuration for GitHub Pages

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/rca-ops-team',
+    images: {
+        unoptimized: true,
+    },
+};
+
+export default nextConfig;
